51b4ea20f68c5b4ce08f080c5b22cd226a9ab626
Paolo Sciarra <paolo.sciarra@lastminute.com>
2019-10-26 20:28:10 +0200

    [refactoring] refactored using foldMap

--- typescript/app/__test__/99BottlesTest.ts
+++ typescript/app/__test__/99BottlesTest.ts
@@ -1,7 +1,11 @@
 import {fold, getFirstMonoid, none, Option, some} from 'fp-ts/lib/Option';
 import {Monoid} from 'fp-ts/lib/Monoid';
+import {foldMap} from "fp-ts/lib/Array";
 
 const monoidAnyOnOption: Monoid<Option<string>> = getFirstMonoid();
+
+type rule = (n: number) => Option<string>
+const applyRuleTo = (n:number) => (f: rule) => f(n);
 
 function compose<A, B, C>(f: (A) => B, g: (B) => C) {
     return (a: A) => g(f(a));
@@ -12,11 +16,11 @@ function capitalize(str: string) {
 }
 
 function currentBottlesOfBeerFrom(n: number): string {
-    let onZeroBottles = (num: number) => num == 0 ?  some(`no more bottles of beer`) : none;
-    let onOneBottle = (num: number) => num == 1 ?  some(`1 bottle of beer`) : none;
-    let edgeCases = [onZeroBottles, onOneBottle]
-        .map(f => f(n))
-        .reduce(monoidAnyOnOption.concat);
+    const onZeroBottles = (num: number) => num == 0 ?  some(`no more bottles of beer`) : none;
+    const onOneBottle = (num: number) => num == 1 ?  some(`1 bottle of beer`) : none;
+
+    const rules = [onZeroBottles, onOneBottle];
+    const edgeCases = foldMap(monoidAnyOnOption)(applyRuleTo(n))(rules);
 
     return fold(() => `${n} bottles of beer`, (s:string) => s)(edgeCases);
 }
@@ -28,61 +32,60 @@ function first_period_from(n: number): string {
 }
 
 function actionFor(bottles_number: number): string {
-    let onZeroRemainingBottles = (n: number) => n == 0 ? some(`Go to the store and buy some more`) : none;
-    let edgeCases: Option<string> = onZeroRemainingBottles(bottles_number);
+    const onZeroRemainingBottles = (n: number) => n == 0 ? some(`Go to the store and buy some more`) : none;
+    const edgeCases: Option<string> = onZeroRemainingBottles(bottles_number);
     return fold(() => `Take one down and pass it around`, (s: string) => s)(edgeCases);
 }
 
 function remaining_bottles_from(n: number): string {
-    let onZeroBottles = (num: number) => num == 0 ? some(`99 bottles`) : none;
-    let onOneBottle = (num: number) => num == 1 ? some(`no more bottles`) : none;
-    let onTwoBottles = (num: number) => num == 2 ? some(`1 bottle`) : none;
+    const onZeroBottles = (num: number) => num == 0 ? some(`99 bottles`) : none;
+    const onOneBottle = (num: number) => num == 1 ? some(`no more bottles`) : none;
+    const onTwoBottles = (num: number) => num == 2 ? some(`1 bottle`) : none;
 
-    let edgeCases = [onZeroBottles, onOneBottle, onTwoBottles]
-        .map(f => f(n))
-        .reduce(monoidAnyOnOption.concat);
+    const rules = [onZeroBottles, onOneBottle, onTwoBottles];
+    const edgeCases = foldMap(monoidAnyOnOption)(applyRuleTo(n))(rules);
 
     return fold(() => `${n - 1} bottles`, (s:string) => s)(edgeCases);
 }
 
 function second_period_from(n: number): string {
-    let action = actionFor(n);
-    return `${action}, ${remaining_bottles_from(n)} of beer on the wall.`;
+    const action = actionFor(n);
+    const motto = `${remaining_bottles_from(n)} of beer on the wall.`;
+    return `${action}, ${motto}`;
 }
 
-function ninety_nine_bottles(n: number): string {
-    return [first_period_from, second_period_from].map(f => f(n)).join('\n');
+function ninety_nine_bottles_verse(n: number): string {
+    return first_period_from(n) + `\n` + second_period_from(n);
 }
 
 describe('99 bottles Kata', () => {
     it('should produce the right 99th verse', () => {
-        expect(ninety_nine_bottles(99))
+        expect(ninety_nine_bottles_verse(99))
             .toEqual(`99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.`);
     });
 
     it('should produce the right 98th verse', () => {
-        expect(ninety_nine_bottles(98))
+        expect(ninety_nine_bottles_verse(98))
             .toEqual(`98 bottles of beer on the wall, 98 bottles of beer.
 Take one down and pass it around, 97 bottles of beer on the wall.`);
     });
 
     it('should produce the right 2nd verse', () => {
-        expect(ninety_nine_bottles(2))
+        expect(ninety_nine_bottles_verse(2))
             .toEqual(`2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.`);
     });
 
     it('should produce the right 1st verse', () => {
-        expect(ninety_nine_bottles(1))
+        expect(ninety_nine_bottles_verse(1))
             .toEqual(`1 bottle of beer on the wall, 1 bottle of beer.
 Take one down and pass it around, no more bottles of beer on the wall.`);
     });
 
     it('should produce the right 0th verse', () => {
-        expect(ninety_nine_bottles(0))
+        expect(ninety_nine_bottles_verse(0))
             .toEqual(`No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.`);
     });
-
 });
