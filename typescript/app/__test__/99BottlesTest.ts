e905c2bfde05b06afae03b9f8e28e9bb8f1258b3
Paolo Sciarra <paolo.sciarra@lastminute.com>
2019-10-26 19:50:14 +0200

    [refactoring] used literal and extracted method

--- typescript/app/__test__/99BottlesTest.ts
+++ typescript/app/__test__/99BottlesTest.ts
@@ -1,47 +1,47 @@
-import {Option, some, none, fold, getFirstMonoid} from 'fp-ts/lib/Option';
+import {fold, getFirstMonoid, none, Option, some} from 'fp-ts/lib/Option';
 import {Monoid} from 'fp-ts/lib/Monoid';
 
+const monoidAnyOnOption: Monoid<Option<string>> = getFirstMonoid();
+
+function compose<A, B, C>(f: (A) => B, g: (B) => C) {
+    return (a: A) => g(f(a));
+
+}
 function capitalize(str: string) {
     return str.charAt(0).toUpperCase() + str.slice(1);
-}
 
+}
 function zero_bottles_currently(bottles_number: number): Option<string> {
     if (bottles_number === 0) {
         return some(`no more bottles of beer`);
     } else {
         return none;
     }
-}
 
+}
 function one_bottle_currently(bottles_number: number): Option<string> {
     if (bottles_number === 1) {
-        return some(`${bottles_number} bottle of beer`);
+        return some('1 bottle of beer');
     } else {
         return none;
     }
-}
 
-const monoidAnyOnOption: Monoid<Option<string>> = getFirstMonoid();
+}
 
 function currentBottlesOfBeerFrom(n: number): string {
     let onNone = () => `${n} bottles of beer`;
     let onSome = s => s;
-    return fold(onNone, onSome)(
-        [zero_bottles_currently, one_bottle_currently]
-            .map(f => f(n))
-            .reduce(monoidAnyOnOption.concat)
-    );
-}
+    let edgeCases = [zero_bottles_currently, one_bottle_currently]
+        .map(f => f(n))
+        .reduce(monoidAnyOnOption.concat);
 
-function compose<A, B, C>(f: (A) => B, g: (B) => C) {
-    return (a: A) => g(f(a));
+    return fold(onNone, onSome)(edgeCases);
 }
 
 function first_period_from(bottles_number: number): string {
     const template: (s: string) => string = (currentBottlesOfBeer: string) =>
         `${currentBottlesOfBeer} on the wall, ${currentBottlesOfBeer}.`;
-    let firstPeriod = compose(currentBottlesOfBeerFrom, template);
-    return compose(firstPeriod, capitalize)(bottles_number);
+    return compose(compose(currentBottlesOfBeerFrom, template), capitalize)(bottles_number);
 }
 
 function actionFor(bottles_number: number): string {
